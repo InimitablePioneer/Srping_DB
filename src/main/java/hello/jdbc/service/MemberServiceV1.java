e2526322c5f8a8116649d8f5c5f8107d315f2cb0
InimitablePioneer <145961960+InimitablePioneer@users.noreply.github.com>
2024-10-17 22:51:03 +0900

    OCT 17 at home via MAC

---
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,29 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 memberRepository;
+
+    public void accountTransfer(String formId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(formId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(formId, fromMember.getMoney() - money);
+        memberRepository.update(toId, toMember.getMoney() + money);
+
+
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+
+        }
+    }
+}
