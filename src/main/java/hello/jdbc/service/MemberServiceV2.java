8006efea250b6716ac6d7995adfe40c7510772d2
InimitablePioneer <seungwonpioneer@gmail.com>
2024-10-22 14:24:48 +0900

    OCT22 at work via WIN

--- src/main/java/hello/jdbc/service/MemberServiceV2.java
+++ src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -1,25 +1,57 @@
 package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepositoryV1;
+import hello.jdbc.repository.MemberRepositoryV2;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
 import java.sql.SQLException;
 
+@Slf4j
 @RequiredArgsConstructor
 public class MemberServiceV2 {
 
-    private final MemberRepositoryV1 memberRepository;
+    private final DataSource dataSource;
+    private final MemberRepositoryV2 memberRepository;
 
     public void accountTransfer(String formId, String toId, int money) throws SQLException {
-        Member fromMember = memberRepository.findById(formId);
-        Member toMember = memberRepository.findById(toId);
+        Connection con = dataSource.getConnection();
+        try {
+            con.setAutoCommit(false); //트랜잭션 시작
 
-        memberRepository.update(formId, fromMember.getMoney() - money);
+            //비즈니스 로직
+            bizLogic(con, formId, toId, money);
+
+            con.commit(); //성공시 커밋
+        } catch (Exception e) {
+            con.rollback(); //실패시 롤백
+            throw new IllegalStateException(e);
+        } finally {
+            release(con);
+        }
+
+    }
+
+    private void bizLogic(Connection con, String formId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(con, formId);
+        Member toMember = memberRepository.findById(con, toId);
+
+        memberRepository.update(con, formId, fromMember.getMoney() - money);
         validation(toMember);
-        memberRepository.update(toId, toMember.getMoney() + money);
+        memberRepository.update(con, toId, toMember.getMoney() + money);
+    }
 
-
+    private static void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true); //이후에 커넥션 풀에 들어갈때 상단의 false 가 유지됨으로 이과정 통해 기본값인 true 로 바꿔 두어야함
+                con.close();
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
     }
 
     private void validation(Member toMember) {
