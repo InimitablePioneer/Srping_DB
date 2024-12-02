1e161ade8f849d300a113c584604748080716c42
InimitablePioneer <seungwonpioneer@gmail.com>
2024-12-02 14:32:25 +0900

    DEC2 at work via WIN

--- src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
+++ src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
@@ -1,10 +1,14 @@
 package hello.jdbc.exception.translator;
 
+import hello.jdbc.connection.ConnectionConst;
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.ex.MyDbException;
 import hello.jdbc.repository.ex.MyDuplicationKeyException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.support.JdbcUtils;
 
 import javax.sql.DataSource;
@@ -13,7 +17,28 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Random;
 
+import static hello.jdbc.connection.ConnectionConst.*;
+
 public class ExTranslatorV1Test {
+
+    Repository repository;
+    Service service;
+
+    @BeforeEach
+    void init() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        repository = new Repository(dataSource);
+        service = new Service(repository);
+    }
+
+    @Test
+    void duplicateKeySave() {
+        service.create("myId");
+        service.create("myId");
+    }
+
+
+
 
 
     @Slf4j
@@ -32,6 +57,9 @@ public class ExTranslatorV1Test {
                 log.info("retryId={}", retryId);
                 repository.save(new Member(retryId, 0));
 
+            } catch (MyDbException e) {
+                log.info("데이터 접근 계층 예외", e);
+                throw e;
             }
 
         }
