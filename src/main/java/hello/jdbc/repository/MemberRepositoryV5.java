00e9207f6c90a24d98f312133b986a51b78c7a40
InimitablePioneer <seungwonpioneer@gmail.com>
2024-12-28 15:39:01 +0900

    Final Update

--- src/main/java/hello/jdbc/repository/MemberRepositoryV5.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV5.java
@@ -55,65 +55,14 @@ public class MemberRepositoryV5 implements MemberRepository{
 
     public void update(String memberId, int money) {
         String sql = "update member set money=? where member_id=?";
+        template.update(sql,money,memberId);
 
-        Connection con = null;
-        PreparedStatement pstmt = null;
-
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
-            pstmt.setInt(1, money);
-            pstmt.setString(2, memberId);
-            int resultSize = pstmt.executeUpdate();
-            log.info("resultSize={}", resultSize);
-        } catch (SQLException e) {
-            //log.error("db error ", e);
-            //throw e;
-            //throw new MyDbException(e);
-            throw exTranslator.translate("update", sql, e);
-        } finally {
-            close(con, pstmt, null);
-        }
     }
-
 
     public void delete(String memberId) {
         String sql = "delete from member where member_id=?";
+        template.update(sql,memberId);
 
-        Connection con = null;
-        PreparedStatement pstmt = null;
-
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, memberId);
-
-            pstmt.executeUpdate();
-
-        } catch (SQLException e) {
-            //log.error("db error", e);
-            //throw e;
-            //throw new MyDbException(e);
-            throw exTranslator.translate("delete", sql, e);
-        } finally {
-            close(con, pstmt, null);
-        }
-    }
-
-    private void close(Connection con, Statement stmt, ResultSet rs) {
-        JdbcUtils.closeResultSet(rs);
-        JdbcUtils.closeStatement(stmt);
-        //JdbcUtils.closeConnection(con);
-        DataSourceUtils.releaseConnection(con, dataSource);
-
-    }
-
-    private Connection getConnection() throws SQLException {
-        //Connection con = dataSource.getConnection();
-        //주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
-        Connection con = DataSourceUtils.getConnection(dataSource);
-        log.info("get connection={}, class={}", con, con.getClass());
-        return con;
     }
 
 }
